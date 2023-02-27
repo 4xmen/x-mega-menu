1c66f558c5f7a6a080f3331221c915c35708d727
A1Gard <a1gard@4xmen.ir>
2023-02-27 23:15:29 +0330

    fix some style sheet fix js bootstrap webpack

--- dist/x-mega-menu.js
+++ dist/x-mega-menu.js
@@ -21,7 +21,7 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
         typeof define === 'function' && define.amd ? define(factory) :
-            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.bootstrap = factory());
+            (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.xMegaMenu = factory());
 })(this, (function () {
     'use strict';
 
