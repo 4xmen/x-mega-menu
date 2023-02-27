c6d5f9cc38a0e748dc20a6760adc6d3c920dfe2f
A1Gard <a1gard@4xmen.ir>
2023-02-28 00:11:22 +0330

    optimize build file

--- src/build.js
+++ src/build.js
@@ -21,8 +21,9 @@ const copyright = `/**
  * @link       https://github.com/4xmen/x-mega-menu
  */
 `;
+
 function execPromise(command) {
-    return new Promise(function(resolve, reject) {
+    return new Promise(function (resolve, reject) {
         exec(command, (error, stdout, stderr) => {
             if (error) {
                 reject(error);
@@ -33,16 +34,53 @@ function execPromise(command) {
         });
     });
 }
+
+function commenting(lines, frm, end) {
+    for (let i = frm; i < end; i++) {
+        lines[i] = '//' + lines[i];
+    }
+    return lines;
+}
+
+function uncommenting(lines, frm, end) {
+    for (let i = frm; i < end; i++) {
+        lines[i] = lines[i].substr(2);
+    }
+    return lines;
+}
+
+async function theme(lines, frm1, end1, frm2, end2, name) {
+    lines = commenting(lines, frm1, end1);
+    lines = uncommenting(lines, frm2 , end2);
+    await fs.writeFileSync('src/_variables.scss', lines.join('\n'));
+    await execPromise('node-sass src/x-mega-menu.scss -o dist');
+    await fs.copyFileSync('dist/x-mega-menu.css', 'dist/theme/x-mega-menu-'+name+'.css');
+    await execPromise('minify dist/theme/x-mega-menu-'+name+'.css > dist/theme/x-mega-menu-'+name+'.min.css');
+}
+
+
 async function Compress() {
-    let src = fs.readFileSync('src/x-mega-menu.js', 'utf8');
-    fs.writeFileSync('dist/x-mega-menu.js', src);
+    // build js
+    let src = await fs.readFileSync('src/x-mega-menu.js', 'utf8');
+    await fs.writeFileSync('dist/x-mega-menu.js', src);
     await execPromise('minify dist/x-mega-menu.js > dist/x-mega-menu.min.js');
+    // build css
+    let vars = await fs.readFileSync('src/_variables.scss', 'utf8');
+    let lines = vars.split('\n');
+
+    await theme(lines,0,10,10,20,'blue');
+    await theme(lines,10,20,20,30,'gray');
+    await theme(lines,20,30,30,40,'dark');
+
+    await fs.writeFileSync('src/_variables.scss', vars);
+    await execPromise('node-sass src/x-mega-menu.scss -o dist');
     await execPromise('minify dist/x-mega-menu.css > dist/x-mega-menu.min.css');
-    await execPromise('minify dist/theme/x-mega-menu-blue.css > dist/theme/x-mega-menu-blue.min.css');
-    await execPromise('minify dist/theme/x-mega-menu-dark.css > dist/theme/x-mega-menu-dark.min.css');
-    await execPromise('minify dist/theme/x-mega-menu-gray.css > dist/theme/x-mega-menu-gray.min.css');
+
     src = fs.readFileSync('dist/x-mega-menu.min.js', 'utf8');
     fs.writeFileSync('dist/x-mega-menu.min.js', copyright + src);
+
+    console.log('- build successfully...');
+
 }
 
 Compress();
